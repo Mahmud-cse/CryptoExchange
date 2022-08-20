2180920bd763855a320f9401b6f96aaeec3c7351
Mahmud-cse <mahmudhasan555666@gmail.com>
2022-08-20 14:02:14 +0600

    Testing Contracts

---
+++ test/Token.js
@@ -0,0 +1,47 @@
+const { ethers } =require("hardhat");
+const { expect } = require("chai");
+
+const tokens=(n)=>{
+	return	ethers.utils.parseUnits(n.toString(),'ether')
+}
+
+describe('Token',()=>{
+    let token;
+
+	beforeEach(async()=>{
+		// Fetch Token from BlockChain
+		const Token = await ethers.getContractFactory('Token');
+		token = await Token.deploy("My Token","MY",1000000);
+	})
+
+	describe('Deployment',()=>{
+		// Tests go inside here.
+		const name='My Token';
+		const symbol='MY';
+		const decimals='18';
+		const totalSupply=tokens(1000000);
+
+		it('has correct name',async()=>{
+			// Read Token Name
+			// const name = await token.name();
+			// Check the name is correct
+			expect(await token.name()).to.equal(name);
+		})
+
+		it('has correct symbol',async()=>{
+			// Read Token Name
+			// const symbol = await token.symbol();
+			// Check the name is correct
+			expect(await token.symbol()).to.equal(symbol);
+		})
+
+		it('has correct decimals',async()=>{
+			expect(await token.decimals()).to.equal(decimals);
+		})
+
+		it('has correct decimals',async()=>{
+			expect(await token.totalSupply()).to.equal(totalSupply);
+		})
+
+	})
+})
